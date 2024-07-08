aac6e956cde2d49fa6bf06e980aa07a0a82ae29e
Dhanush <81028546+dhanush-2313@users.noreply.github.com>
2024-07-08 17:37:35 +0530

    Update landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Sastha Video call</h2>
+                    <h2>Meet</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -34,7 +34,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by sastha Video Call</p>
+                    <p>Cover a distance by Meet</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
